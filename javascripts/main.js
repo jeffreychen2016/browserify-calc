8af5b82578d2a23970d703400cf738a0f5735d83
jeffrey chen <jeffreychen2016@gmail.com>
2018-04-10 20:13:34 -0500

    Adding more math

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const add = require('./add');
+const substract = require('./substract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log('Adding:',add(1,2));
+console.log('Substracting',substract(40,2));
+console.log('Multiplying',multiply(40,1));
+console.log('Dividing',divide(40,2));
